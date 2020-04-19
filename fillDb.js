538305ff0e58765115994cf4edd7063b510c17ca
jarrodwatts <35651410+jarrodwatts@users.noreply.github.com>
2020-04-19 12:15:40 +1000

    Basics of Comps page

--- fillDb.js
+++ fillDb.js
@@ -1,5 +1,4 @@
 const fetch = require('isomorphic-unfetch');
-
 const RIOT_API_KEY = "RGAPI-ab6fe2c4-9d06-4763-b5e3-0c7ff535c19e"
 
 const MongoClient = require('mongodb').MongoClient;
@@ -7,25 +6,25 @@ const uri = "mongodb+srv://jarrod:AwPz9DLa0izkzHnA@matchcluster-tdshe.mongodb.ne
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
-    //if (err) throw err;
-    const collection = client.db("matchDatabase").collection("matchCollection");
+    const collection = client.db("matchDatabase").collection("matchCollection3");
 
-    //1. Grab top 200 players
-    //https://oc1.api.riotgames.com/tft/league/v1/challenger?
+    //1. Grab top challenger players
     fetch(
         `https://oc1.api.riotgames.com/tft/league/v1/challenger` + '?api_key=' + RIOT_API_KEY
     )
         .then((r) => r.json())
         .then(
             (data) => {
+
                 let challengers = data;
                 let puuids = [];
                 let matchIds = [];
                 let matchDetails = [];
 
                 //Loop through each Challenger
-                for (let x = 0; x < 4; x++) {
-                    //console.log(challengers.entries[x].summonerName)
+                for (let x = 0; x < challengers.entries.length; x++) {
+
+                    console.log("Loop", x)
 
                     //Make a call to puuid API with this challenger
                     fetch(
@@ -36,15 +35,14 @@ client.connect(err => {
                             //push the puuid to puuids
                             puuids.push(data.puuid)
 
+
                             //After we do that, make a request with the current puuid to grab this puuid's matches
-                            fetch(`https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/${data.puuid.toString()}/ids?count=20&` + 'api_key=' + RIOT_API_KEY)
+                            fetch(`https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/${data.puuid}/ids?count=20&` + 'api_key=' + RIOT_API_KEY)
                                 .then((r) => r.json())
                                 .then((data) => {
-                                    //push the matches to matchIds
-                                    //by looping through each item of the array
 
-                                    //temp 1 loop which means we'll do 4 summoners x 1 game = 4 results
-                                    for (let i = 0; i < 2; i++) {
+                                    //Loop do all challengers x 20 games
+                                    for (let i = 0; i < 20; i++) {
                                         matchIds.push(data[i]);
                                         //console.log("length:", matchIds.length);
 
@@ -54,29 +52,22 @@ client.connect(err => {
                                                 //console.log("DATA:", data)
                                                 matchDetails.push(data)
 
-                                                console.log("Summoner", x, ", Match", i, matchDetails[i]);
-
+                                                console.log("Summoner", x, ", Match", i, data);
                                                 //Add to MongoDB this match/"data"
-                                                collection.insertOne(data);
-                                                
+                                                collection.insertOne(data)
+                                                    .then(result => {
+                                                        setTimeout(function () { console.log(`Successfully inserted item with _id: ${result.insertedId}`) }, 5000)
+                                                    })
+                                                    .catch(err => {
+                                                        setTimeout(function () { console.error(`Failed to insert item: ${err}`) })
+
+                                                    })
                                             })
                                     }
-
-                                    //With each matchid, make a request to the match details API
-                                    // for (let m = 0; m < 2; m++) {
-
-                                    // }
-
                                 })
                         })
-                    // .then(console.log(matchIds))
+                        .catch((err) => console.log(err))
                 }
-
             })
 })
 
-
-// //get All summoner Id's from challengers
-// let challengerSummonerIds = challengers.entries.map((challenger) => { challenger.summonerId });
-
-// console.log(challengerSummonerIds);
--- pages/Comps.js
+++ pages/Comps.js
@@ -9,32 +9,161 @@ function fetcher(url) {
     return fetch(url).then(r => r.json());
 }
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minWidth: 205,
+        minHeight: 175,
+    },
+
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+    },
+
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+
+    topSpacing: {
+        paddingTop: theme.spacing(2)
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+    },
+}));
+
 export default function Comps() {
 
+    const classes = useStyles();
     const { data, error } = useSWR('/api/getMatches', fetcher);
     const matches = data;
 
-    console.log(error)
-
-    console.log(matches)
-
-    if (!error) {
-        return (
-            <div>
-                <NavBar />
-                <Typography>{matches == undefined ?
-                    "Loading..." :
-                    matches[0].matchName}</Typography>
-            </div>
-        )
-    }
+    if (error) return <div>failed to load</div>
+    if (!matches) return <div>loading...</div>
 
     else {
+
+        let filteredMatches = matches.filter(x => x.metadata != undefined)
+        let masterArray = [];
+
+        //1. For each match,
+        for (let i = 0; i < filteredMatches.length; i++) {
+            //2. Get each player's units array
+            for (let x = 0; x < filteredMatches[i].info.participants.length; x++) {
+                //3. Sort the units array
+
+                let tempArr = filteredMatches[i].info.participants[x].units.sort(function (a, b) {
+                    var x = a.character_id.toLowerCase();
+                    var y = b.character_id.toLowerCase();
+                    if (x < y) { return -1; }
+                    if (x > y) { return 1; }
+                    return 0;
+                });
+
+                //let tempArr = filteredMatches[i].info.participants[x].units.sort(function (a, b) { return a.character_id - b.character_id });
+
+                //4. Push the units array to master array
+                masterArray.push(tempArr);
+            }
+        }
+
+        console.log(masterArray);
+
         return (
             <div>
                 <NavBar />
-                <div>oof ouchie my API broke</div>
+                {/* Main Container */}
+                <Container className={classes.topSpacing}>
+
+                    <Grid container spacing={3} direction="column" >
+                        {/* Heading and subtitle */}
+                        <Grid container item direction="column" justify="center" alignItems="center">
+                            <Typography variant="h2">LOLPROJECT</Typography>
+                            <Typography variant="h5">Teamfight Tactics Compositions</Typography>
+                        </Grid>
+
+                        {/* Filters */}
+                        <Grid container item>
+
+                        </Grid>
+
+                        {/* Champions */}
+                        <Grid container item xs={12} direction="column" spacing={1}>
+                            {filteredMatches.map((match, key) => (
+
+                                <Box key={key} style={{ paddingBottom: '16px' }}>
+
+                                    {/* Begin individual Match Papers */}
+                                    <Grid item>
+                                        <Paper className={classes.paper}>
+                                            <Grid container item direction="row" alignItems="center" justify="space-between" spacing={3}>
+
+                                                <Grid item>
+                                                    <Grid container direction="row" alignItems="center">
+
+                                                        {/* Companion image */}
+                                                        <Grid item style={{ paddingLeft: '8px' }}>
+                                                            {/* temp blitz: TODO: replace with companion icon */}
+                                                            <Avatar src={`/assets/champions/blitzcrank.png`} />
+                                                        </Grid>
+
+                                                        {/* Placement and Type */}
+                                                        <Grid item style={{ paddingLeft: '16px' }}>
+                                                            <Box>
+                                                                <Typography><b>placement</b></Typography>
+                                                            </Box>
+                                                        </Grid>
+
+                                                        {/* Synergies / Traits */}
+                                                        <Grid item style={{ paddingLeft: '64px' }}>
+                                                            <Typography>synergies</Typography>
+                                                            {/* <Grid container direction="row">
+                                                                {match.info.participants[getParticipantsIndex(match, profile.puuid)].traits.map((trait, key) => (
+                                                                    <Grid item key={key}>
+                                                                        {renderSynergy(trait, classes)}
+                                                                    </Grid>
+                                                                ))}
+                                                            </Grid> */}
+                                                        </Grid>
+
+                                                    </Grid>
+                                                </Grid>
+
+
+                                                {/* Champs */}
+                                                <Grid item >
+                                                    <Grid container direction="row" alignItems="center">
+                                                        <Typography>Champs</Typography>
+                                                        {/* {match.info.participants[getParticipantsIndex(match, profile.puuid)].units.map((unit, key) => (
+                                                            <Grid item key={key}>
+                                                                <Avatar
+                                                                    src={`/assets/champions/${sliceCharacterString(unit.character_id)}.png`}
+                                                                    className={classes.large} />
+                                                            </Grid>
+                                                        ))} */}
+                                                    </Grid>
+                                                </Grid>
+
+                                            </Grid>
+
+                                        </Paper>
+                                    </Grid>
+                                </Box>
+                            ))}
+
+                        </Grid>
+
+
+
+                    </Grid>
+                </Container>
             </div>
         )
     }
+
 }
